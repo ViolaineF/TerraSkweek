dbee7639d5f402262b295b32d5b243c76159959e
EdouardAngebault <edouard.angebault@gmail.com>
2015-10-22 11:33:14 +0200

    animation player working well

--- Game/VS_Project/TerraSkweek/TerraSkweek/Player.cpp
+++ Game/VS_Project/TerraSkweek/TerraSkweek/Player.cpp
@@ -365,7 +365,7 @@ void Player::Draw()
 		}
 	}
 
-	if (m_firing)
+	else if (m_firing)
 	{
 		if ((m_life < 6 && m_life > 3))
 		{
@@ -604,32 +604,12 @@ void Player::Draw()
 		}
 	}
 	
-	else{}
-	glPushMatrix();
-	// Left
-	glEnable(GL_TEXTURE_2D);
-	glEnable(GL_BLEND);
-	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-	glBindTexture(GL_TEXTURE_2D, idle[0]);
-	glBegin(GL_QUADS);
-	glColor3d(1.0, 1.0, 1.0);
-	glTexCoord2f(1.0f, 1.0f);
-	glVertex2d(m_pos.x, m_pos.y);
-	glTexCoord2f(0.0f, 1.0f);
-	glVertex2d(m_pos.x + 1, m_pos.y);
-	glTexCoord2f(0.0f, 0.0f);
-	glVertex2d(m_pos.x + 1, m_pos.y + 1);
-	glTexCoord2f(1.0f, 0.0f);
-	glVertex2d(m_pos.x, m_pos.y + 1);
-	glEnd();
-	glDisable(GL_TEXTURE_2D);
-	glPopMatrix();
 }
 
 Player::Player() : Entity()
 {
 	m_pos = { 4,4,0 };
-	m_life = 10;
+	m_life = 5;
 	m_def = 10;
 	m_dir = 'l';
 	m_speed = 0.1;
